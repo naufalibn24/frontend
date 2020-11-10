56ca6a191c3bc9ad4459c29d887492a271f7e359
petrus766hi <petrus.sigiro@gmail.com>
2020-11-11 06:51:04 +0700

    fist-comit

--- src/app/shell/header/header.service.ts
+++ src/app/shell/header/header.service.ts
@@ -4,7 +4,7 @@ import { Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
 const routes = {
-  kategori: () => `http://localhost:3000/api/kategori/`,
+  kategori: () => `https://backends-petrus.herokuapp.com/api/kategori/`,
 };
 
 @Injectable({
